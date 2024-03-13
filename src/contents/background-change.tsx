7c5a81f8090ad20141071f4e238bc1dc7f8f4ff3
Gia Huy <21520909@gm.uit.edu.vn>
2024-03-13 18:59:48 +0700

    ft: delete theme

--- src/contents/background-change.tsx
+++ src/contents/background-change.tsx
@@ -1,21 +1,29 @@
 import type { PlasmoCSConfig } from "plasmo"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://courses.uit.edu.vn/"],
+  matches: ["https://courses.uit.edu.vn/*"],
   all_frames: true
 }
 
 const BackgroundChange = async () => {
-  //   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  //     if (request.message === "draculaTheme") {
-  ColorChange()
-  //     }
-  //   })
+  chrome.storage.sync.get(["draculaTheme"], (result) => {
+    if (result.draculaTheme) {
+      ColorChange()
+    }
+  })
+
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === "color_change") {
+      ColorChange()
+    }
+    if (request.message === "revert_changes") {
+      RevertChanges()
+    }
+  })
 }
 
-const ColorChange = () => {
+const ColorChange = async () => {
   document.body.style.backgroundColor = "#282a36"
-
   let regionMain = document.getElementById("region-main") as HTMLElement
   regionMain.style.backgroundColor = "#282a36"
   regionMain.style.color = "#f8f8f2"
@@ -43,6 +51,38 @@ const ColorChange = () => {
   }
   for (let i = 0; i < cardTitleElements.length; i++) {
     cardTitleElements[i].style.color = "#bd93f9"
+  }
+}
+
+const RevertChanges = async () => {
+  document.body.style.backgroundColor = ""
+  let regionMain = document.getElementById("region-main") as HTMLElement
+  regionMain.style.backgroundColor = ""
+  regionMain.style.color = ""
+  regionMain.style.border = ""
+
+  let courseNames = document.getElementsByClassName(
+    "coursename"
+  ) as HTMLCollectionOf<HTMLElement>
+  for (let i = 0; i < courseNames.length; i++) {
+    courseNames[i].style.color = ""
+  }
+
+  let pageHeaderHeading = document.getElementsByClassName(
+    "page-header-headings"
+  )[0] as HTMLElement
+  let cardBodyElements = document.getElementsByClassName(
+    "card-body"
+  ) as HTMLCollectionOf<HTMLElement>
+  let cardTitleElements = document.getElementsByClassName(
+    "card-title"
+  ) as HTMLCollectionOf<HTMLElement>
+  pageHeaderHeading.style.color = ""
+  for (let i = 0; i < cardBodyElements.length; i++) {
+    cardBodyElements[i].style.backgroundColor = ""
+  }
+  for (let i = 0; i < cardTitleElements.length; i++) {
+    cardTitleElements[i].style.color = ""
   }
 }
 
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -4,15 +4,32 @@ import "./style.css"
 
 function IndexPopup() {
   const handleClick = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { message: "draculaTheme" })
+    chrome.storage.sync.set({ draculaTheme: true }, () => {
+      console.log('The "draculaTheme" setting has been saved.')
+    })
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { message: "color_change" })
+    })
+  }
+
+  const handleDelete = () => {
+    chrome.storage.sync.set({ draculaTheme: false }, () => {
+      console.log('The "draculaTheme" setting has been deleted.')
+    })
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { message: "revert_changes" })
     })
   }
 
   return (
     <div className="w-64 h-64">
-      <button onClick={handleClick} className="w-5 h-5 bg-red-500">
-        Click
+      <button onClick={handleClick} className="button mb-9">
+        Dracula
+      </button>
+      <button onClick={handleDelete} className="button">
+        Delete theme
       </button>
     </div>
   )
