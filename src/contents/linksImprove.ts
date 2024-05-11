042e40dedc5e7f70360919d0b7c910c818ff0113
Shoru <bapcui596@gmail.com>
2024-05-11 16:15:03 +0700

    fix: added more pathways for  setting to work upon login

--- src/contents/linksImprove.ts
+++ src/contents/linksImprove.ts
@@ -4,17 +4,19 @@ import { StudentImproveLinksSettingsName } from "./../options/components/student
 import SettingWatcher from "./generalSettingsWatcher"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://student.uit.edu.vn/"],
+  matches: [
+    "https://student.uit.edu.vn/",
+    "https://student.uit.edu.vn/user/login%26homepage"
+  ],
   all_frames: true
 }
+
 const extractValue = (option: HTMLOptionElement) => option.value
 const extractText = (option: HTMLOptionElement) => option.innerText
+
 const selectBox = document.querySelector(".content div .links")
 const options = selectBox?.querySelectorAll("option")
-//Get from select box parent
 const oldContainer = selectBox?.parentElement
-console.log(oldContainer)
-//Extract value from options
 
 const links = Array.from(options || []).map((option) => ({
   text: extractText(option),
@@ -38,36 +40,34 @@ function injectLinks() {
 }
 
 function linkComponent(link: { text: string; value: string }) {
-  //Create a bigger than old box that is easily selectable
   const container = document.createElement("div")
-  container.classList.add("links-container")
-  //Create a link
+  container.classList.add("link-container")
   const linkElement = document.createElement("a")
 
-  linkElement.innerText = link.text.replace("--", "")
+  linkElement.innerText = `${link.text.replace("--", "")}`
   linkElement.href = link.value
-  linkElement.style.display = "block"
   linkElement.style.padding = "10px"
 
   container.appendChild(linkElement)
   return container
 }
+
 function injectLinkStyle() {
   const style = document.createElement("style")
   style.innerHTML = `
     .links-container {
       display: flex;
       flex-direction: column;
-      gap: 10px;
     }
-    .links-container a {
+    .link-container a {
       text-decoration: none;
-      color: #0056e0
-      padding: 5x;  
-      border-radius: 5px;
-      border: 1px solid #f0f0f0;
+      color: #0056e0;
+      display: flex;
+      align-items: center;
+      border-bottom: 1px solid #ddd;
+      font-size: 14px;
     }
-    .links-container a:hover {
+    .link-container a:hover {
       background-color: #f0f0f0;
     }
     `
