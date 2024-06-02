96d377e57532b08282cccd984373a4bdf87a085e
Shoru <bapcui596@gmail.com>
2024-06-03 00:55:37 +0700

    chore: added guard to prevent browser warning

--- src/contents/removeUnnecessarySections.tsx
+++ src/contents/removeUnnecessarySections.tsx
@@ -1,27 +1,43 @@
 import type { PlasmoCSConfig } from "plasmo"
 import { useEffect } from "react"
 
+import { CalendarSettingName } from "~src/options/components/moodle/courseCalendarOptions"
+
+import SettingWatcher from "./generalSettingsWatcher"
+
 export const config: PlasmoCSConfig = {
   matches: ["https://courses.uit.edu.vn/"],
   all_frames: true
 }
 
 const RemoveUnnecessarySectionsCS = () => {
+  console.log("RemoveUnnecessarySectionsCS")
   // Remove website introduction
-  const unnecessarySections = document.getElementById("inst2")
-  unnecessarySections.remove()
-
+  const unnecessarySections = document.getElementById("inst2") as HTMLElement
+  if (unnecessarySections) {
+    unnecessarySections.remove()
+  }
   //remove online lately
-  const onlineLately = document.getElementById("inst31")
-  onlineLately.remove()
+  const onlineLately = document.getElementById("inst31") as HTMLElement
+  if (onlineLately) {
+    onlineLately.remove()
+  }
 
   const calendarContent = document
     .getElementById("inst3")
     .getElementsByClassName("card-text content mt-3")[0]
 
   // Remove footer
-  const footer = calendarContent.getElementsByClassName("footer")[0]
-  footer.remove()
+  const footer = calendarContent.getElementsByClassName(
+    "footer"
+  )[0] as HTMLElement
+  if (footer) {
+    footer.remove()
+  }
 }
 
-RemoveUnnecessarySectionsCS()
+SettingWatcher.get(CalendarSettingName).then((calendarEnabled) => {
+  if (calendarEnabled === "true") {
+    RemoveUnnecessarySectionsCS()
+  }
+})
